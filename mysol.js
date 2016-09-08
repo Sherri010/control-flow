b494333058bdbbb228f7d6c362100143d9773a05
sherri010 <aminshahidi.sh@gmail.com>
2016-09-08 09:02:23 -0700

    my solution

---
+++ mysol.js
@@ -0,0 +1,15 @@
+var joke= { isfunny:"yes"};
+
+switch(joke.isfunny()){
+	case "yes":
+		laugh();
+		break;
+	case "no" :
+	     stare();
+	     break;
+	case "sort of":
+	     chartle();
+	     break;     	
+
+
+	  }
